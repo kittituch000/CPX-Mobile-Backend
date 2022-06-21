9bad87a6dabef431f2f917d39b373d8bde28e35e
kitttituch <kittituch@magicboxasia.com>
2022-06-21 17:59:16 +0700

    question8 and 9
    about get method

--- spring-test/src/main/java/com/testspring/springtest/controller/testSpringBoot.java
+++ spring-test/src/main/java/com/testspring/springtest/controller/testSpringBoot.java
@@ -3,9 +3,15 @@ package com.testspring.springtest.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.testspring.springtest.repository.UsersDao;
+
+import antlr.collections.List;
+
 import com.testspring.springtest.model.Users;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -26,6 +32,16 @@ public class testSpringBoot {
         userDao.save(user);
         return "insert success";
 
+    }
+
+    @GetMapping("/questions8/{id}")
+    public Object Questions8(@PathVariable String id) {    
+        return  userDao.findById(Integer.parseInt(id));
+    }
+
+    @GetMapping("/questions9")
+    public Object Questions9() {    
+        return  userDao.findAll();
     }
     
     
--- spring-test/src/main/java/com/testspring/springtest/model/Users.java
+++ spring-test/src/main/java/com/testspring/springtest/model/Users.java
@@ -5,9 +5,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.Setter;
+
 
 
 @Entity
+@Setter
+@Getter
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
